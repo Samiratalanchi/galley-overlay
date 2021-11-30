322502c889cf64d73c0f6cdad41adcfdce5a0d0e
samira talanchi <samira73talanchi@gmail.com>
2021-11-30 23:47:09 +0330

    add cell overlay

---
+++ myjs.js
@@ -0,0 +1,8 @@
+// function borderShow() {
+//     document.getElementById("viewBox").style.animation="border 1s linear forwards";
+//     document.getElementsByClassName(".viewBox")[0].style.animation="border 1s linear forwards";
+//     document.querySelectorAll(".viewBox")[0].style.animation="border 1s linear forwards";
+// }
+// function hideBorder() {
+//     document.getElementById("viewBox").style.animation="none";
+// }
