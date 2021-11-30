ca9daabe4e06fd3d9ba8481a42b4cd0b66454333
samira talanchi <samira73talanchi@gmail.com>
2021-11-30 23:48:22 +0330

    remove unuse files

--- myjs.js
+++
@@ -1,8 +0,0 @@
-// function borderShow() {
-//     document.getElementById("viewBox").style.animation="border 1s linear forwards";
-//     document.getElementsByClassName(".viewBox")[0].style.animation="border 1s linear forwards";
-//     document.querySelectorAll(".viewBox")[0].style.animation="border 1s linear forwards";
-// }
-// function hideBorder() {
-//     document.getElementById("viewBox").style.animation="none";
-// }
